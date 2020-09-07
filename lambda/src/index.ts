62da499d5e31508b96620b2a14a403e19c0523fa
Wojtek Lichota <wojtek@adtonos.com>
2020-09-07 07:33:42 +0000

    reformat

--- lambda/src/index.ts
+++ lambda/src/index.ts
@@ -13,33 +13,40 @@ import { YoursTrulyRequestHandler } from './YoursTrulyHandler';
 import { Constants } from './Constants';
 
 export async function handler(event: RequestEnvelope, context: any, callback: any): Promise<void> {
-  const factory = SkillBuilders.standard().addRequestHandlers(
-    CheckAudioInterfaceHandler,
-    new SkillEventHandler(),
-    RadioRequestHandler.builder().withHandlers(IntentHandler).withHandlers(AudioHandler).build(),
-    new YoursTrulyRequestHandler()
-  );
 
-  const skill = factory.create();
+    const factory = SkillBuilders.standard()
+        .addRequestHandlers(
+            CheckAudioInterfaceHandler,
+            new SkillEventHandler(),
+            new YoursTrulyRequestHandler(),
+            RadioRequestHandler.builder()
+                .withHandlers(IntentHandler)
+                .withHandlers(AudioHandler)
+                .build()
+        );
 
-  try {
-    if (Constants.debug) {
-      console.log('\n' + '******************* REQUEST  **********************');
-      console.log(JSON.stringify(event, null, 2));
+    const skill = factory.create();
+
+    try {
+
+        if (Constants.debug) {
+            console.log("\n" + "******************* REQUEST  **********************");
+            console.log(JSON.stringify(event, null, 2));
+        }
+
+        const responseEnvelope: ResponseEnvelope = await skill.invoke(event, context);
+
+        if (Constants.debug) {
+            console.log("\n" + "******************* RESPONSE  **********************");
+            console.log(JSON.stringify(responseEnvelope, null, 2));
+        }
+
+        return callback(null, responseEnvelope);
+
+    } catch (error) {
+        if (Constants.debug) {
+            console.log(JSON.stringify(error, null, 2));
+        }
+        return callback(error);
     }
-
-    const responseEnvelope: ResponseEnvelope = await skill.invoke(event, context);
-
-    if (Constants.debug) {
-      console.log('\n' + '******************* RESPONSE  **********************');
-      console.log(JSON.stringify(responseEnvelope, null, 2));
-    }
-
-    return callback(null, responseEnvelope);
-  } catch (error) {
-    if (Constants.debug) {
-      console.log(JSON.stringify(error, null, 2));
-    }
-    return callback(error);
-  }
 }
